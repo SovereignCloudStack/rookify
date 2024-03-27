ccb807b93412f7496ff223f365563858216037b2
Tobias Wolf <wolf@b1-systems.de>
2024-03-27 09:25:08 +0100

    Add YAML config schema and validation
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- src/rookify/yaml.py
+++ src/rookify/yaml.py
@@ -1,14 +1,26 @@
 # -*- coding: utf-8 -*-
 
+import yamale
 import yaml
+from importlib import resources
+from pathlib import Path
 from typing import Any, Dict
 
 
+_schema_file = Path("rookify", "config.schema.yaml")
+for entry in resources.files("rookify").iterdir():
+    if entry.name == "config.schema.yaml":
+        _schema_file = entry
+
+
 def load_yaml(path: str) -> Dict[str, Any]:
-    with open(path, "r") as file:
-        data = yaml.safe_load(file)
-        assert isinstance(data, dict)
-        return data
+    schema = yamale.make_schema(_schema_file)
+    data = yamale.make_data(path)
+
+    yamale.validate(schema, data)
+
+    assert isinstance(data[0][0], dict)
+    return data[0][0]
 
 
 def save_yaml(path: str, data: Dict[str, Any]) -> None:
