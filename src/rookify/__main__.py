62bb79fdac67782c91f7010517f5ef5a6594b71d
Tobias Wolf <wolf@b1-systems.de>
2024-05-01 19:08:52 +0200

    Differentiate between config and module data files
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- src/rookify/__main__.py
+++ src/rookify/__main__.py
@@ -4,12 +4,12 @@ import os
 import rookify.modules
 
 from types import MappingProxyType
-from .yaml import load_yaml, save_yaml
+from .yaml import load_config, load_module_data, save_module_data
 
 
 def main() -> None:
     try:
-        config = load_yaml("config.yaml")
+        config = load_config("config.yaml")
     except FileNotFoundError as err:
         raise SystemExit(f"Could not load config: {err}")
     preflight_modules, migration_modules = rookify.modules.load_modules(
@@ -18,7 +18,7 @@ def main() -> None:
 
     module_data = dict()
     try:
-        module_data.update(load_yaml(config["general"]["module_data_file"]))
+        module_data.update(load_module_data(config["general"]["module_data_file"]))
     except FileNotFoundError:
         pass
 
@@ -57,7 +57,7 @@ def main() -> None:
         result = handler.run()
         module_data[migration_module.MODULE_NAME] = result
 
-    save_yaml(config["general"]["module_data_file"], module_data)
+    save_module_data(config["general"]["module_data_file"], module_data)
 
 
 if __name__ == "__main__":
--- src/rookify/yaml.py
+++ src/rookify/yaml.py
@@ -8,16 +8,16 @@ from pathlib import Path
 from typing import Any, Dict
 
 
-_schema_file: Path | importlib.resources.abc.Traversable = Path(
+_config_schema_file: Path | importlib.resources.abc.Traversable = Path(
     "rookify", "config.schema.yaml"
 )
 for entry in importlib.resources.files("rookify").iterdir():
     if entry.name == "config.schema.yaml":
-        _schema_file = entry
+        _config_schema_file = entry
 
 
-def load_yaml(path: str) -> Dict[str, Any]:
-    schema = yamale.make_schema(_schema_file)
+def load_config(path: str) -> Dict[str, Any]:
+    schema = yamale.make_schema(_config_schema_file)
     data = yamale.make_data(path)
 
     yamale.validate(schema, data)
@@ -26,6 +26,13 @@ def load_yaml(path: str) -> Dict[str, Any]:
     return data[0][0]
 
 
-def save_yaml(path: str, data: Dict[str, Any]) -> None:
+def load_module_data(path: str) -> Dict[str, Any]:
+    with open(path, "r") as file:
+        data = yaml.safe_load(file)
+        assert isinstance(data, dict)
+        return data
+
+
+def save_module_data(path: str, data: Dict[str, Any]) -> None:
     with open(path, "w") as file:
         yaml.safe_dump(data, file)
