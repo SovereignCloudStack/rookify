6aa972c458d2a713af6f2255cb461d857ef4f95e
Tobias Wolf <NotTheEvilOne@users.noreply.github.com>
2024-05-06 14:44:34 +0200

    Add `cephx` auth config module
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

---
+++ src/rookify/modules/cephx_auth_config/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# type: ignore
+
+from .main import CephXAuthHandler
+
+MODULE_NAME = "cephx_auth_config"
+HANDLER_CLASS = CephXAuthHandler
+REQUIRES = []
+AFTER = []
+PREFLIGHT_REQUIRES = []
