99e29eae2a1a6ba415eec0d8b3e2fc6e3113d605
Tobias Wolf <NotTheEvilOne@users.noreply.github.com>
2024-05-29 11:30:20 +0200

    Use "transitions" as the workflow handler
    * Add "transitions" based execution machine
    * Adapt existing modules for "transitions"
    * Split "preflight" and "execution" transition states and store state data
    * Remove `PREFLIGHT_REQUIRES` logic
    * Fix execution if no `machine_pickle_file` is configured
    * Fix access to preflight state in `migrate_osds`
    * Fix docstring for class `ModuleHandler`
    
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- src/rookify/modules/cephx_auth_config/main.py
+++ src/rookify/modules/cephx_auth_config/main.py
@@ -1,16 +1,37 @@
 # -*- coding: utf-8 -*-
 
-from ..module import ModuleHandler
 from typing import Any
+from ..machine import Machine
+from ..module import ModuleException, ModuleHandler
 
 
 class CephXAuthHandler(ModuleHandler):
-    def run(self) -> Any:
-        self.logger.debug("Reconfiguring Ceph to expect cephx auth")
+    def preflight(self) -> Any:
+        if not self.is_cephx_set(self.ceph.conf_get("auth_cluster_required")):
+            raise ModuleException(
+                "Ceph config value auth_cluster_required does not contain cephx"
+            )
 
-        self.ceph.conf_set("auth_cluster_required", "cephx")
-        self.ceph.conf_set("auth_service_required", "cephx")
-        self.ceph.conf_set("auth_client_required", "cephx")
+        if not self.is_cephx_set(self.ceph.conf_get("auth_service_required")):
+            raise ModuleException(
+                "Ceph config value auth_service_required does not contain cephx"
+            )
 
-        self.logger.info("Reconfigured Ceph to expect cephx auth")
-        return {"reconfigured": True}
+        if not self.is_cephx_set(self.ceph.conf_get("auth_client_required")):
+            raise ModuleException(
+                "Ceph config value auth_client_required does not contain cephx"
+            )
+
+        self.machine.get_preflight_state("CephXAuthHandler").verified = True
+        self.logger.info("Validated Ceph to expect cephx auth")
+
+    def is_cephx_set(self, values: str) -> Any:
+        return "cephx" in [value.strip() for value in values.split(",")]
+
+    @staticmethod
+    def register_preflight_state(
+        machine: Machine, state_name: str, handler: ModuleHandler, **kwargs: Any
+    ) -> None:
+        ModuleHandler.register_preflight_state(
+            machine, state_name, handler, tags=["verified"]
+        )
