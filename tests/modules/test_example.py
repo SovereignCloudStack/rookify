ab8f3090a2c86a030ff0507ce6d88eaf34339c23
Rafael te Boekhorst <boekhorst@b1-systems.de>
2024-06-11 17:20:05 +0200

    fix: make example runnable as a class with unittest
    Signed-off-by: Rafael te Boekhorst <boekhorst@b1-systems.de>

--- tests/modules/test_example.py
+++ tests/modules/test_example.py
@@ -1,12 +1,13 @@
 # -*- coding: utf-8 -*-
 
-import pytest
+import unittest
 
 from rookify.modules.example.main import ExampleHandler
 from rookify.modules.machine import Machine
 from rookify.modules.module import ModuleException
 
 
-def test_preflight() -> None:
-    with pytest.raises(ModuleException):
-        ExampleHandler(Machine(), {}).preflight()
+class TestExampleHandler(unittest.TestCase):
+    def test_preflight(self) -> None:
+        with self.assertRaises(ModuleException):
+            ExampleHandler(Machine(), {}).preflight()
