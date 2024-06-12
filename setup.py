3d4cd233ebd985122fdea4b86a71cfacf9e58c49
Tobias Wolf <wolf@b1-systems.de>
2024-06-12 09:05:40 +0200

    Fix default rookify dev version
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ def get_version() -> str:
     :since:  v0.0.1
     """
 
-    return os.environ.get("ROOKIFY_VERSION", "0.0.0.dev0")
+    return os.environ.get("ROOKIFY_VERSION", "0.0.0.dev1")
 
 
 _setup: Dict[str, Any] = {
