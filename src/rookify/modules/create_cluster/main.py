4a0256761bccc87624dbb42446ae8544058553ef
Tobias Wolf <wolf@b1-systems.de>
2024-06-12 09:15:44 +0200

    Fix comparision to `None` in `CreateClusterHandler`
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- src/rookify/modules/create_cluster/main.py
+++ src/rookify/modules/create_cluster/main.py
@@ -147,7 +147,7 @@ class CreateClusterHandler(ModuleHandler):
 
         watcher.stop()
 
-        if result == None:
+        if result is None:
             raise ModuleException("CephCluster did not come up")
 
     @staticmethod
