f5985296df73461a24ebf6a4af0ca157248eecec
Rafael te Boekhorst <boekhorst@b1-systems.de>
2024-06-26 14:10:45 +0200

    fix: adapting tests and adding return types to sort_pickle_file
    Signed-off-by: Rafael te Boekhorst <boekhorst@b1-systems.de>

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,5 +1,5 @@
 import sys
-from typing import Callable, Optional
+from typing import Any, Callable, Optional
 import pytest
 from _pytest.monkeypatch import MonkeyPatch
 import yaml
@@ -8,7 +8,7 @@ import pytest_structlog
 
 from argparse import Namespace
 
-from rookify.__main__ import parse_args, main
+from rookify.__main__ import parse_args, main, sort_pickle_file
 import rookify.yaml
 
 
@@ -41,8 +41,8 @@ def test_parse_args_no_flags() -> None:
 
 
 @pytest.fixture  # type: ignore
-def mock_load_config(monkeypatch: MonkeyPatch) -> Callable[[Optional[str]], MagicMock]:
-    def _mock_load_config(pickle_file: Optional[str] = None) -> MagicMock:
+def mock_load_config(monkeypatch: MonkeyPatch) -> Callable[[Optional[Any]], MagicMock]:
+    def _mock_load_config(pickle_file: Optional[Any] = None) -> MagicMock:
         # Mock the configuration data
         # Load config.example.yaml
         with open("config.example.yaml", "r") as file:
@@ -62,7 +62,7 @@ def mock_load_config(monkeypatch: MonkeyPatch) -> Callable[[Optional[str]], Magi
 @pytest.fixture  # type: ignore
 def mock_load_pickler(monkeypatch: MonkeyPatch) -> MagicMock:
     # Mock states_data from pickle file
-    mock_states_data = {"mock_key": "mock_value"}
+    mock_states_data = {"teststuff": {"data": {"mock_key": "mock_value"}}}
 
     # Mock load_pickler function
     mock = MagicMock(return_value=mock_states_data)
@@ -72,12 +72,12 @@ def mock_load_pickler(monkeypatch: MonkeyPatch) -> MagicMock:
 
 
 def test_main_show_progress(
-    mock_load_config: Callable[[Optional[str]], MagicMock],
+    mock_load_config: Callable[[Optional[Any]], MagicMock],
     mock_load_pickler: MonkeyPatch,
     monkeypatch: MonkeyPatch,
     log: pytest_structlog.StructuredLogCapture,
 ) -> None:
-    # Load mock.pickle as a configuration file
+    # Load example config with mock.pickle as pickle file
     mock_load_config("mock.pickle")
 
     # Mock sys.argv to simulate command-line arguments
@@ -87,10 +87,13 @@ def test_main_show_progress(
     main()
 
     # Verify logging messages
-    assert log.has("Pickle file set: mock.pickle")
-    assert log.has(
-        "Current state as retrieved from pickle-file: {'mock_key': 'mock_value'}"
+    expected_log_message = (
+        "Current state as retrieved from pickle-file: {\n"
+        '    "mock_key": "mock_value"\n'
+        "}"
     )
+    assert log.has("Pickle file set: mock.pickle", level="info")
+    assert log.has(expected_log_message, level="info")
 
 
 def test_main_no_pickle_file(
@@ -110,3 +113,79 @@ def test_main_no_pickle_file(
 
     # Assertions
     assert log.has("No pickle file was set in the configuration.")
+
+
+def test_sort_pickle_file() -> None:
+    #  Prepare unsorted data
+    unsorted_states_data = getUnsortedData()
+
+    # Expected keys order
+    expected_order = ["device", "fs", "mon", "node", "osd", "ssh"]
+
+    # Run sort_pickle_file
+    sorted_states_data = sort_pickle_file(unsorted_states_data)
+
+    # Assert that order is correct
+    sorted_states_data_keys = list(sorted_states_data.keys())
+
+    assert expected_order == sorted_states_data_keys
+
+
+def getUnsortedData() -> Any:
+    unsorted_states_data = {
+        "preflighttestdata": {
+            "data": {
+                "mon": {
+                    "dump": {"epoch": 1},
+                    "mons": [{"rank": 0}],
+                    "quorum": [0, 1, 2],
+                },
+                "fs": {"dump": {"epoch": 1}},
+                "device": {"ls": ["devid", 1]},
+                "osd": {
+                    "dump": [{"epoch": 138}],
+                    "osds": [{"osd": 0}],
+                    "osd_xinfo": [{"osd": 0}],
+                },
+                "node": {
+                    "ls": {
+                        "mon": {
+                            "test-node-0": ["test-node-0"],
+                            "test-node-1": ["test-node-1"],
+                            "test-node-2": ["test-node-2"],
+                        },
+                        "osd": {
+                            "test-node-0": [2, 3],
+                            "test-node-1": [0, 5],
+                            "test-node-2": [1, 4],
+                        },
+                        "mds": {
+                            "test-node-0": ["test-node-0"],
+                            "test-node-1": ["test-node-1"],
+                            "test-node-2": ["test-node-2"],
+                        },
+                        "mgr": {
+                            "test-node-0": ["test-node-0"],
+                            "test-node-1": ["test-node-1"],
+                            "test-node-2": ["test-node-2"],
+                        },
+                    }
+                },
+                "ssh": {
+                    "osd": {
+                        "test-node-0": {
+                            "devices": ["/dev/ceph-bla-1", "/dev/ceph-bla-2"]
+                        },
+                        "test-node-1": {
+                            "devices": ["/dev/ceph-bla-3", "/dev/ceph-bla-4"]
+                        },
+                        "test-node-2": {
+                            "devices": ["/dev/ceph-bla-5", "/dev/ceph-bla-6"]
+                        },
+                    }
+                },
+            }
+        }
+    }
+
+    return unsorted_states_data
