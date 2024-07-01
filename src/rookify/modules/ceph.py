b5ab57192061b68564d3c5d8b803c08e3d659ac5
Tobias Wolf <wolf@b1-systems.de>
2024-07-01 14:08:07 +0200

    Add additional logic to the `Ceph` module for reusability
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- src/rookify/modules/ceph.py
+++ src/rookify/modules/ceph.py
@@ -19,9 +19,7 @@ class Ceph:
     def __getattr__(self, name: str) -> Any:
         return getattr(self.__ceph, name)
 
-    def _json_command(
-        self, handler: Any, *args: List[Any]
-    ) -> Dict[str, Any] | List[Any]:
+    def _json_command(self, handler: Any, *args: Any) -> Dict[str, Any] | List[Any]:
         result = handler(*args)
         if result[0] != 0:
             raise ModuleException(f"Ceph did return an error: {result}")
@@ -34,14 +32,35 @@ class Ceph:
 
         return data
 
+    def get_osd_pool_configurations_from_osd_dump(
+        self, dump_data: Dict[str, Any]
+    ) -> Dict[str, Any]:
+        osd_pools = {osd_pool["pool_name"]: osd_pool for osd_pool in dump_data["pools"]}
+
+        erasure_code_profiles = dump_data["erasure_code_profiles"]
+
+        for osd_pool_name in osd_pools:
+            osd_pool = osd_pools[osd_pool_name]
+
+            osd_pool["erasure_code_configuration"] = erasure_code_profiles.get(
+                osd_pool["erasure_code_profile"], erasure_code_profiles["default"]
+            )
+
+        return osd_pools
+
     def mon_command(self, command: str, **kwargs: str) -> Dict[str, Any] | List[Any]:
         cmd = {"prefix": command, "format": "json"}
         cmd.update(**kwargs)
-        return self._json_command(self.__ceph.mon_command, json.dumps(cmd), b"")  # type: ignore
+        return self._json_command(self.__ceph.mon_command, json.dumps(cmd), b"")
+
+    def mgr_command(self, command: str, **kwargs: str) -> Dict[str, Any] | List[Any]:
+        cmd = {"prefix": command, "format": "json"}
+        cmd.update(**kwargs)
+        return self._json_command(self.__ceph.mgr_command, json.dumps(cmd), b"")
 
     def osd_command(
         self, osd_id: int, command: str, **kwargs: str
     ) -> Dict[str, Any] | List[Any]:
         cmd = {"prefix": command, "format": "json"}
         cmd.update(**kwargs)
-        return self._json_command(self.__ceph.osd_command, osd_id, json.dumps(cmd), b"")  # type: ignore
+        return self._json_command(self.__ceph.osd_command, osd_id, json.dumps(cmd), b"")
