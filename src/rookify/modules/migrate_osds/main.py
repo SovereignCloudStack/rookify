a3ee1f91e4dad27ba2d892fa697fc7bb8f9fa700
Tobias Wolf <wolf@b1-systems.de>
2024-08-26 21:31:59 +0200

    Handle OSDs migrations based on the OSD host
    Fixes: #70
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- src/rookify/modules/migrate_osds/main.py
+++ src/rookify/modules/migrate_osds/main.py
@@ -10,12 +10,12 @@ from ..module import ModuleHandler
 class MigrateOSDsHandler(ModuleHandler):
     REQUIRES = ["migrate_mons"]
 
-    def _get_devices_of_hosts(self) -> Dict[str, List[str]]:
+    def _get_devices_of_hosts(self) -> Dict[str, Dict[str, str]]:
         state_data = self.machine.get_preflight_state("AnalyzeCephHandler").data
-        osd_devices: Dict[str, List[str]] = {}
+        osd_devices: Dict[str, Dict[str, str]] = {}
 
         for osd_host, osds in state_data["node"]["ls"]["osd"].items():
-            osd_devices[osd_host] = []
+            osd_devices[osd_host] = {}
 
             """
             Read OSD metadata to get OSD fsid UUID.
@@ -39,10 +39,30 @@ class MigrateOSDsHandler(ModuleHandler):
 
                 osd_device_path = "/dev/ceph-{0}/osd-block-{0}".format(osd_vg_name)
 
-                if osd_device_path not in osd_devices[osd_host]:
-                    osd_devices[osd_host].append(osd_device_path)
+                if osd_id not in osd_devices[osd_host]:
+                    osd_devices[osd_host][osd_id] = osd_device_path
 
         return osd_devices
+
+    def _get_nodes_osd_devices(self, osd_ids: List[str]) -> List[Dict[str, Any]]:
+        osd_host_devices = self.machine.get_preflight_state(
+            "MigrateOSDsHandler"
+        ).osd_host_devices
+
+        filtered_osd_host_devices: Dict[str, List[Dict[str, str]]] = {}
+
+        for host, devices in osd_host_devices.items():
+            for osd_id, device_path in devices.items():
+                if osd_id in osd_ids:
+                    if host not in filtered_osd_host_devices:
+                        filtered_osd_host_devices[host] = []
+
+                    filtered_osd_host_devices[host].append({"name": device_path})
+
+        return [
+            {"name": host, "devices": devices}
+            for host, devices in filtered_osd_host_devices.items()
+        ]
 
     def preflight(self) -> None:
         osd_host_devices = self.machine.get_preflight_state_data(
@@ -59,40 +79,22 @@ class MigrateOSDsHandler(ModuleHandler):
         ).osd_host_devices = self._get_devices_of_hosts()
 
     def execute(self) -> None:
-        if self.machine.get_execution_state_data(
-            "MigrateOSDsHandler", "migrated", default_value=False
-        ):
-            return
-
-        osd_host_devices = self.machine.get_preflight_state(
-            "MigrateOSDsHandler"
-        ).osd_host_devices
-
-        nodes_osd_devices = [
-            {"name": host, "devices": [{"name": device} for device in devices]}
-            for host, devices in osd_host_devices.items()
-        ]
-
-        cluster_patch_templated = self.load_template(
-            "nodes_osd_devices_patch.yaml.j2",
-            cluster_namespace=self._config["rook"]["cluster"]["namespace"],
-            cluster_name=self._config["rook"]["cluster"]["name"],
-            nodes_osd_devices_list=nodes_osd_devices,
+        osd_host_devices = self.machine.get_preflight_state_data(
+            "MigrateOSDsHandler", "osd_host_devices", default_value=[]
         )
 
-        crd_api = self.k8s.crd_api(api_version="ceph.rook.io/v1", kind="CephCluster")
-
-        crd_api.patch(
-            content_type="application/merge-patch+json",
-            body=cluster_patch_templated.yaml,
-        )
+        state_data = self.machine.get_preflight_state("AnalyzeCephHandler").data
 
         for host in osd_host_devices.keys():
-            self._migrate_osd_node(host)
+            self.migrate_osds(host, state_data["node"]["ls"]["osd"][host])
 
-        self.machine.get_execution_state("MigrateOSDsHandler").migrated = True
+    def migrate_osds(self, host: str, osd_ids: List[int]) -> None:
+        migrated_osd_ids = self.machine.get_execution_state_data(
+            "MigrateOSDsHandler", "migrated_osd_ids", default_value=[]
+        )
 
-    def _migrate_osd_node(self, host: str) -> None:
+        self.logger.info("Migrating Ceph OSD host '{0}'".format(host))
+
         node_patch = {"metadata": {"labels": {self.k8s.osd_placement_label: "true"}}}
 
         if (
@@ -103,10 +105,11 @@ class MigrateOSDsHandler(ModuleHandler):
                 "Failed to patch k8s node for Ceph OSD host '{0}'".format(host)
             )
 
-        state_data = self.machine.get_preflight_state("AnalyzeCephHandler").data
+        for osd_id in osd_ids:
+            if osd_id in migrated_osd_ids:
+                return
 
-        for osd_id in state_data["node"]["ls"]["osd"][host]:
-            self.logger.debug(
+            self.logger.info(
                 "Migrating Ceph OSD daemon '{0}@{1:d}'".format(host, osd_id)
             )
 
@@ -137,11 +140,35 @@ class MigrateOSDsHandler(ModuleHandler):
                 sleep(2)
 
             self.logger.info(
-                "Disabled Ceph OSD daemon '{0}@{1:d}' and enabling Rook based Ceph OSD daemon".format(
-                    host, osd_id
-                )
+                "Disabled Ceph OSD daemon '{0}@{1:d}'".format(host, osd_id)
             )
 
+        self.logger.info("Enabling Rook based Ceph OSD host '{0}'".format(host))
+
+        nodes_osd_devices = self._get_nodes_osd_devices(migrated_osd_ids + osd_ids)
+
+        cluster_patch_templated = self.load_template(
+            "nodes_osd_devices_patch.yaml.j2",
+            cluster_namespace=self._config["rook"]["cluster"]["namespace"],
+            cluster_name=self._config["rook"]["cluster"]["name"],
+            nodes_osd_devices_list=nodes_osd_devices,
+        )
+
+        crd_api = self.k8s.crd_api(api_version="ceph.rook.io/v1", kind="CephCluster")
+
+        crd_api.patch(
+            content_type="application/merge-patch+json",
+            body=cluster_patch_templated.yaml,
+        )
+
+        for osd_id in osd_ids:
+            migrated_osd_ids.append(osd_id)
+
+        self.machine.get_execution_state(
+            "MigrateOSDsHandler"
+        ).migrated_osd_ids = migrated_osd_ids
+
+        for osd_id in osd_ids:
             self.logger.debug(
                 "Waiting for Rook based OSD daemon '{0}@{1:d}'".format(host, osd_id)
             )
@@ -163,7 +190,7 @@ class MigrateOSDsHandler(ModuleHandler):
         machine: Machine, state_name: str, handler: ModuleHandler, **kwargs: Any
     ) -> None:
         ModuleHandler.register_execution_state(
-            machine, state_name, handler, tags=["migrated"]
+            machine, state_name, handler, tags=["migrated_osd_ids"]
         )
 
     @staticmethod
