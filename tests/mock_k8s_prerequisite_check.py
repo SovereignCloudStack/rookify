28f5dfdcbbc3acacbd29b41690de52648fe72b1d
Boekhorst <boekhorst@b1-systems.de>
2024-09-09 12:33:53 +0200

    feat: add unit-test to check for empty response in list_deployment_for_all_namespaces()
    Signed-off-by: Boekhorst <boekhorst@b1-systems.de>

--- tests/mock_k8s_prerequisite_check.py
+++ tests/mock_k8s_prerequisite_check.py
@@ -6,10 +6,10 @@ from .mock_k8s import MockK8s
 
 
 # Note: currently this test works with pytest but not with unittest, which is not able to import needed classes
-class MockK8sPrerequisitesCheckHandler(K8sPrerequisitesCheckHandler):
+class MockK8sPrerequisitesCheckHandler(K8sPrerequisitesCheckHandler):  # type: ignore
     def __init__(self, request_callback: Any, *args: Any, **kwargs: Any) -> None:
         K8sPrerequisitesCheckHandler.__init__(self, *args, **kwargs)
-        self._k8s = MockK8s(request_callback)  # type: ignore
+        self._k8s = MockK8s(request_callback)
 
     def execute(self) -> None:
         K8sPrerequisitesCheckHandler.execute(self)
--- tests/modules/test_k8s_prerequisites_check.py
+++ tests/modules/test_k8s_prerequisites_check.py
@@ -11,11 +11,15 @@ class TestK8sPrerequisitesCheckHandler(unittest.TestCase):
     def setUp(self) -> None:
         # Mock configuration
         self.config = {"rook": {"cluster": {"namespace": "test-namespace"}}}
+        # No response option
+        self.empty_response = False
 
     def _request_callback(
         self, method: str, *args: List[Any], **kwargs: Dict[Any, Any]
     ) -> Any:
         if method == "apps_v1_api.list_deployment_for_all_namespaces":
+            if self.empty_response is True:
+                return MockResponse([])
             return MockResponse(["apple", "banana", "cherry"])
         if method == "core_v1_api.list_namespace":
             return self._mock_list_namespace()
@@ -48,6 +52,19 @@ class TestK8sPrerequisitesCheckHandler(unittest.TestCase):
         )
         # Set the k8s attribute to the mock_k8s instance
         handler_instance.preflight()
+
+    def test_list_deployment_for_all_namespaces_fails(self) -> None:
+        # Set no response
+        self.empty_response = True
+
+        # Instantiate K8sPrerequisitesCheckHandler with the mock ModuleHandler
+        handler_instance = MockK8sPrerequisitesCheckHandler(
+            self._request_callback, Mock(), self.config
+        )
+
+        # Call the preflight method to run the test
+        with self.assertRaises(ModuleException):
+            handler_instance.preflight()
 
     def test_namespaces_fails(self) -> None:
         # Instantiate K8sPrerequisitesCheckHandler with the mock ModuleHandler
