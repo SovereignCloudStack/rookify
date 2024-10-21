b745bcdaef277efadf74b66493eb3e061ec4c9cf
Tobias Wolf <NotTheEvilOne@users.noreply.github.com>
2024-10-21 16:32:15 +0200

    Handle access to undefined states gracefully
    Fixes: #76
    Signed-off-by: Tobias Wolf <wolf@b1-systems.de>

--- src/rookify/modules/migrate_mds_pools/main.py
+++ src/rookify/modules/migrate_mds_pools/main.py
@@ -40,9 +40,9 @@ class MigrateMdsPoolsHandler(ModuleHandler):
                     if pool_data_osd_name not in migrated_pools:
                         migrated_pools.append(pool_data_osd_name)
 
-                self.machine.get_execution_state(
-                    "MigrateMdsPoolsHandler"
-                ).migrated_pools = migrated_pools
+                state = self.machine.get_execution_state("MigrateMdsPoolsHandler")
+                if state is not None:
+                    state.migrated_pools = migrated_pools
 
                 continue
 
